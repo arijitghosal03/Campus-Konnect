5c930cc60e55c05295db9fb245977b36fae6f2f9
arijit03 <arijitghosal0309@gmail.com>
2025-06-14 01:03:15 +0530

    interview room worked

--- frontend/src/pages/interview.tsx
+++ frontend/src/pages/interview.tsx
@@ -371,11 +371,16 @@ const InterviewRoom: React.FC = () => {
     { urls: 'stun:stun2.l.google.com:19302' },
     { urls: 'stun:stun3.l.google.com:19302' },
     { urls: 'stun:stun4.l.google.com:19302' },
-    
-    // Additional reliable STUN servers
-    { urls: 'stun:stun.nextcloud.com:443' },
-    { urls: 'stun:stun.sipgate.net:3478' },
-    { urls: 'stun:stun.ekiga.net' },
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
     
     // Xirsys free STUN (backup)
     { urls: 'stun:global.stun.twilio.com:3478' }
